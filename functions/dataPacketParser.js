9e3ca43e7480c5e7e55cb62ee6e56278c2ed0718
Raghav <raghav.dhingra15@gmail.com>
2020-05-11 17:09:56 +0530

    dataParser

--- functions/dataPacketParser.js
+++ functions/dataPacketParser.js
@@ -2,10 +2,19 @@ const converter = require("hex2dec");
 
 const dataPacketParser = (data) => {
   let obj = {};
-  let Zeroes = converter.hexToDec(Buffer.from(data).toString("hex", 0, 4));
-  console.log(Zeroes);
+  let four_zeroes = converter.hexToDec(Buffer.from(data).toString("hex", 0, 4));
+  let data_field_length = converter.hexToDec(
+    Buffer.from(data).toString("hex", 4, 8)
+  );
+  let codec_id = converter.hexToDec(Buffer.from(data).toString("hex", 8, 9));
+  let data_1_number = converter.hexToDec(
+    Buffer.from(data).toString("hex", 9, 10)
+  );
   obj = {
-    data: data,
+    four_zeroes: four_zeroes,
+    data_field_length: data_field_length,
+    codec_id: codec_id,
+    data_1_number: data_1_number,
   };
   return obj;
 };
